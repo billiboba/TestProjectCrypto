912712901fe5e54711bddeb3eb636f4cf3dc4b0f
Roman <romakor.10@yandex.ru>
2024-05-31 15:26:07 +0700

    Добавьте файлы проекта.

---
+++ TestProjectCrypto/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TestProjectCrypto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.test = new System.Windows.Forms.Label();
+            this.BybitListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // test
+            // 
+            this.test.AutoSize = true;
+            this.test.Location = new System.Drawing.Point(3, -1);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(98, 13);
+            this.test.TabIndex = 0;
+            this.test.Text = "Current BTC Price: ";
+            // 
+            // BybitListBox
+            // 
+            this.BybitListBox.FormattingEnabled = true;
+            this.BybitListBox.Location = new System.Drawing.Point(214, -1);
+            this.BybitListBox.Name = "BybitListBox";
+            this.BybitListBox.Size = new System.Drawing.Size(75, 628);
+            this.BybitListBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1133, 617);
+            this.Controls.Add(this.BybitListBox);
+            this.Controls.Add(this.test);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label test;
+        private System.Windows.Forms.ListBox BybitListBox;
+    }
+}
+
---
+++ TestProjectCrypto/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Bybit.Net.Clients;
+using Timer = System.Windows.Forms.Timer;
+
+namespace TestProjectCrypto
+{
+    public partial class Form1 : Form
+    {
+
+        private BybitSocketClient _socketClient;
+        private BybitRestClient _bybitClient;
+        private Timer _timer;
+        public Form1()
+        {
+            InitializeComponent();
+            Load += new EventHandler(Form1_Load);
+            _bybitClient = new BybitRestClient();
+            _timer = new Timer();
+            _timer.Interval = 5000;
+            _timer.Tick += new EventHandler(Timer_Tick);
+            _timer.Start();
+            BybitListBox.SelectedIndexChanged += new EventHandler(ListBoxCryptos_SelectedIndexChanged);
+        }
+
+        private async void Timer_Tick(object sender, EventArgs e)
+        {
+            string selectedCrypto = BybitListBox.SelectedItem.ToString();
+            await GetPrice(selectedCrypto);
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await GetSymbol();
+        }
+
+        protected async Task GetSymbol()
+        {
+            try
+            {
+                var result = await _bybitClient.SpotApiV3.ExchangeData.GetSymbolsAsync();
+                if (result.Success)
+                {
+                    BybitListBox.Items.Clear();
+                    List<string> cryptoNames = new List<string>();
+                    foreach (var symbol in result.Data)
+                    {
+                        cryptoNames.Add(symbol.BaseAsset);
+                    }
+                    cryptoNames = cryptoNames.Distinct().ToList();
+                    BybitListBox.Items.AddRange(cryptoNames.ToArray());
+                }
+                else
+                {
+                    MessageBox.Show("Failed to retrieve data");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+
+        private void ListBoxCryptos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (BybitListBox.SelectedItem != null)
+            {
+                string selectedCrypto = BybitListBox.SelectedItem.ToString();
+                GetPrice(selectedCrypto);
+            }
+        }
+
+        protected async Task GetPrice(string symbol)
+        {
+            try
+            {
+                string tradingPair = $"{symbol}USDT";
+                var result = await _bybitClient.SpotApiV3.ExchangeData.GetTickerAsync(tradingPair);
+
+                if (result.Success)
+                {
+                    var price = result.Data.LastPrice;
+                    test.Text = $"Current {symbol} Price: {price} USDT";
+                }
+                else
+                {
+                    MessageBox.Show("Failed to retrieve data");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
